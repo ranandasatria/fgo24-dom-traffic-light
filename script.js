3a4f1050c916bc1e57c66dd2bebe67269d3dabb6
ranandasatria <rananda24@gmail.com>
2025-05-07 14:28:09 +0700

    refactor: change the traffic algorithm

--- script.js
+++ script.js
@@ -2,25 +2,51 @@ const merah = document.querySelector('.merah');
 const kuning = document.querySelector('.kuning');
 const hijau = document.querySelector('.hijau');
 
-function turnOnRed() {
-  merah.classList.add('on');
-  kuning.classList.remove('on');
-  hijau.classList.remove('on');
-  setTimeout(turnOnYellow, 3000);
+
+function turnOnLight(lampu, durasi) {
+    merah.classList.remove('on')
+    kuning.classList.remove('on')
+    hijau.classList.remove('on')
+    
+    lampu.classList.add('on')
+
+    setTimeout(() => {
+        if(lampu === merah) {
+            turnOnLight(kuning, 2000)
+        }else if (lampu === kuning) {
+            turnOnLight(hijau, 3000)
+        }else {
+            turnOnLight(merah, 3000)
+        }
+    }, durasi);
 }
 
-function turnOnYellow() {
-  merah.classList.remove('on');
-  kuning.classList.add('on');
-  hijau.classList.remove('on');
-  setTimeout(turnOnGreen, 2000);
-}
+turnOnLight(merah, 3000)
+  
 
-function turnOnGreen() {
-  merah.classList.remove('on');
-  kuning.classList.remove('on');
-  hijau.classList.add('on');
-  setTimeout(turnOnRed, 3000);
-}
 
-turnOnRed();
+
+
+// function turnOnRed() {
+//   merah.classList.add('on');
+//   kuning.classList.remove('on');
+//   hijau.classList.remove('on');
+//   setTimeout(turnOnYellow, 3000);
+// }
+
+// function turnOnYellow() {
+//   merah.classList.remove('on');
+//   kuning.classList.add('on');
+//   hijau.classList.remove('on');
+//   setTimeout(turnOnGreen, 2000);
+// }
+
+// function turnOnGreen() {
+//   merah.classList.remove('on');
+//   kuning.classList.remove('on');
+//   hijau.classList.add('on');
+//   setTimeout(turnOnRed, 3000);
+// }
+
+// turnOnRed();
+
